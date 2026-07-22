b614eb4a7c59d4c71a5ff15568e2b19f9de545c0
Chan Myae Aung <2000chanmyae@gmail.com>
2026-07-22 09:30:07 +0630

    template engine test

--- expressServer.js
+++ expressServer.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
-
+const path=require("path");
+app.set("views","./view");
+app.set("view engine","ejs");
 app.use((req, res, next) => {
     console.log("Middleware Running...");
    console.log(req.url);
@@ -10,12 +12,21 @@ app.use((req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-    res.send("Home Page");
+    // res.send("Home Page");
+    // res.sendFile(__dirname +"/view/home.html");
+//     res.sendFile(path.join(__dirname,"view","home.html"));
+// 
+res.render("home")
 });
 app.get("/about", (req, res) => {
-    res.send("about Page");
+    // res.send("about Page");
+    //  res.sendFile(__dirname +"/view/about.html");
+    //   res.sendFile(path.join(__dirname,"view","about.html"));
+    res.render('about')
 });
 app.use((req,res)=>{
-    res.send("404 page")
+    // res.send("404 page")
+    //  res.sendFile(__dirname +"/view/404.html");
+    res.render("404")
 })
 app.listen(3000);
