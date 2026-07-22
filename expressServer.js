71434b4ef89b35d0e6df1c52470e5ae6b4f93010
Chan Myae Aung <2000chanmyae@gmail.com>
2026-07-22 10:19:26 +0630

    redirect route test

--- expressServer.js
+++ expressServer.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 const path=require("path");
-app.set("views","./view");
-app.set("view engine","ejs");
+// app.set("views","./view");
+// app.set("view engine","ejs");
 app.use((req, res, next) => {
     console.log("Middleware Running...");
    console.log(req.url);
@@ -11,22 +11,25 @@ app.use((req, res, next) => {
     
 });
 
-app.get("/", (req, res) => {
+app.get(["/","/home","/homepage"], (req, res) => {
     // res.send("Home Page");
     // res.sendFile(__dirname +"/view/home.html");
-//     res.sendFile(path.join(__dirname,"view","home.html"));
+    res.sendFile(path.join(__dirname,"view","home.html"));
 // 
-res.render("home")
+// res.render("home")
 });
 app.get("/about", (req, res) => {
     // res.send("about Page");
     //  res.sendFile(__dirname +"/view/about.html");
-    //   res.sendFile(path.join(__dirname,"view","about.html"));
-    res.render('about')
+      res.sendFile(path.join(__dirname,"view","about.html"));
+    // res.render('about')
+});
+app.get("/about-usPage", (req, res) => {
+    res.redirect("/about");
 });
 app.use((req,res)=>{
     // res.send("404 page")
-    //  res.sendFile(__dirname +"/view/404.html");
-    res.render("404")
+     res.sendFile(__dirname +"/view/404.html");
+    // res.render("404")
 })
 app.listen(3000);
